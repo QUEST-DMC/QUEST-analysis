e1e48ea69ffdf1fbdb17039b71bd5b8411a5ec21
Tineke Salmon <tinekesalmon@Tinekes-MacBook-Pro.local>
2023-02-24 14:16:40 +0000

    force vel data now with fitting database

--- Lancaster/Fridge4/Calibration/get_data_week2_nonlinear_amp_sweep_f4wires.py
+++ Lancaster/Fridge4/Calibration/get_data_week2_nonlinear_amp_sweep_f4wires.py
@@ -12,7 +12,6 @@ import f4wire001 as f4wire
 # 00:24 - 00:32 10022023
 t1=1675988640
 t2=1675989120
-t0=1675988640
 
 n=0
 for cell in (1, 2):
@@ -20,6 +19,18 @@ for cell in (1, 2):
   nnh = 'w%dbh'%(cell)
   nnt = 'w%dbt'%(cell)
   
-  # data
+  # f4wire (drive current and conv voltage) data
   h_wire_data = f4wire.get_data(nnh, t1,t2, use_bg=1, cnv_drive=1, cnv_volt=1, cache='data/' + '%s' %nnh + '_f4wire_get_data_' + '%d' %t1 + '_%d' %t2 )
   t_wire_data = f4wire.get_data(nnt, t1,t2, use_bg=1, cnv_drive=1, cnv_volt=1, cache='data/' + '%s' %nnt + '_f4wire_get_data_' + '%d' %t1 + '_%d' %t2 )
+
+  # get_range fitting database for width and fitting parameters 
+
+  # heater data
+  # + '_sweeps' gives raw data. just wire name (i.e. nnh) gives fitting results
+  h_data = graphene.get_range(nnh, t1,t2,  cache='data/' + '%s' %nnh + '_get_range_' + '%d' %t1 + '_%d' %t2)
+  #_dbox:f1 gives the filter 1 of dbox which is the conversion factor between the drive generator voltage (V) and drive current (A)
+  h_Rbox, h_att = graphene.get_prev(nnh + '_dbox', t1, unpack=1, usecols=(1,2), cache='data/' + '%s' %nnh + '_dbox_get_prev_' + '%d' %t1 + '_%d' %t2)
+
+  # thermometer
+  t_data= graphene.get_range(nnt, t1, t2, cache='data/' + '%s' %nnt + '_get_range_' + '%d' %t1 + '_%d' %t2)
+  t_Rbox, t_att = graphene.get_prev(nnt + '_dbox', t1, unpack=1, usecols=(1,2), cache='data/' + '%s' %nnt + '_dbox_get_prev_' + '%d' %t1 + '_%d' %t2)
